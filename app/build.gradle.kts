bdab188479458cf7cbc1b68248433e8659e958dd
Your Name <your-email@example.com>
2025-03-26 02:52:51 +0530

    rhet

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,7 +42,6 @@ dependencies {
     androidTestImplementation(libs.espresso.core)
 
     implementation ("com.android.volley:volley:1.2.1")
-
     implementation ("com.github.bumptech.glide:glide:4.15.1")
     implementation ("androidx.cardview:cardview:1.0.0")
 }
